ad9fd6309e0695a3ce63e5beacfb0a87b1ef4e1c
Muhd Alip Tajudin <mr.alif.93@gmail.com>
2017-04-22 15:23:05 +0800

    Updated By Alip (DB, Register)

--- DB.php
+++ DB.php
@@ -1,17 +1,18 @@
 <?php
 
-    $DB_HOST = "locahost";
+    $DB_HOST = "localhost";
     $DB_USER = "root";
     $DB_PASS = "";
-    $DB_DATABASE = "";
+    $DB_DATABASE = "RL_db";
 
     // Create Connection
     $conn = mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
     mysql_select_db($DB_DATABASE);
 
     // Check connection
-    if ($conn) {
-        die("Connection failed: " . mysql_error());
-    } 
-    echo "Connected successfully";
+    if (!$conn) {
+        die("Cannot connect to database!");
+    }
+    
+    return $conn;
 ?>
---
+++ register.php
@@ -0,0 +1,35 @@
+<?php
+
+include("DB.php");
+
+if(isset($_POST["username"]) && isset($_POST["password"]))
+{
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $sql = "INSERT INTO tbl_user (username, password) VALUES ('$username', '$password')";
+    
+    $result = mysql_query($sql) or die(mysql_error());
+    
+    if(!$result)
+    {
+        echo "Cannot add new user";
+    }
+    
+    echo "Successfully add new user..";
+}
+
+?>
+
+
+<html>
+    <head><title>Register/Login Tutorial GitHub</title></head>
+    <body>
+        <h1>Register User</h1>
+        <form action="" method="post">
+            Username : <input type="text" name="username" placeholder="Username"> <br>
+            Password : <input type="text" name="password" placeholder="Password"> <br>
+            <button type="submit">Submit</button><button type="reset">Reset</button>
+        </form>
+    </body>
+</html>
